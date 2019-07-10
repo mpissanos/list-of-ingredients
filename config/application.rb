4e4b58de2236c71209928157aae632cb4f8a055e
mpissanos <mpissano@gmail.com>
2019-07-10 04:51:17 -0400

    added middleware for Rack & actiondispatch to application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module ListOfIngredients
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use Rack::MethodOverride
+config.middleware.use ActionDispatch::Flash
+config.middleware.use ActionDispatch::Cookies
+config.middleware.use ActionDispatch::Session::CookieStore
+
   end
 end
